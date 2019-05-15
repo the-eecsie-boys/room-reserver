6e418d1bd0bb01bc78e807450f8dae6cacbff9ca
George M <geogeo357@gmail.com>
2019-05-14 20:04:47 -0500

    working map

--- RF.js
+++ RF.js
@@ -1,7 +1,12 @@
 function setFloor(floorNum){
   var imageSrc = "Images/tech" + floorNum + ".jpg";
-  var mapDivHTML = `
-        <img id="MapPic" width="100%" src="${imageSrc}">
+  var mapDivHTML = `<img id="MapPic" usemap="#floorMap" width="100%" src="${imageSrc}">`;
+  if(floorNum == 1){
+    mapDivHTML += `<map name="floorMap">
+                      <area shape="rect" coords="0,403,230,638" href="RF3rdPage.html" alt="Wing A">
+                   </map>`;
+  }
+  mapDivHTML += `
         <button type="button" class="btn btn-primary back-btn"><i class="material-icons">navigate_before</i></button>
         <button type="button" class="btn btn-primary next-btn"><i class="material-icons">navigate_next</i></button>
       `;
