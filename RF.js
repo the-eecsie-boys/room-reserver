2682539a25637cec26d6da4caed67b0baec6fa47
George M <geogeo357@gmail.com>
2019-05-14 19:45:35 -0500

    map for floor 1 shows up

--- RF.js
+++ RF.js
@@ -1,12 +1,9 @@
 function setFloor(floorNum){
   var imageSrc = "Images/tech" + floorNum + ".jpg";
-  <img id="MapPic" src="Images/tech1st.jpg">
-</div>
-<div class="Buttons">
-  <a href="RFHomePage.html">
-    <img class = "backButton" src="Images/back.jpg" alt="back button">
-  </a>
-  <a href="RF3rdPage.html">
-    <img class= "forwardButton" src="Images/forward.jpg" alr="forward button">
-  </a>
+  var mapDivHTML = `
+        <img id="MapPic" width="100%" src="${imageSrc}">
+        <button type="button" class="btn btn-primary back-btn"><i class="material-icons">navigate_before</i></button>
+        <button type="button" class="btn btn-primary next-btn"><i class="material-icons">navigate_next</i></button>
+      `;
+  document.getElementById("maps-div").innerHTML = mapDivHTML;
 }
