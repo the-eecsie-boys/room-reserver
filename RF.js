3d5271f6e0c1b5bd07990e455e6788c4a4826170
George M <geogeo357@gmail.com>
2019-05-22 03:28:22 -0500

    Moved new changes to right branch for p7

--- RF.js
+++ RF.js
@@ -8,7 +8,48 @@ function setFloor(floorNum){
   }
   mapDivHTML += `
         <button type="button" onclick="window.location='index.html';" class="btn btn-primary back-btn"><i class="material-icons" >navigate_before</i></button>
-        <button type="button" class="btn btn-primary next-btn"><i class="material-icons">navigate_next</i></button>
+        <button type="button" onclick="window.location='RF3rdPage.html';" class="btn btn-primary next-btn"><i class="material-icons">navigate_next</i></button>
       `;
   document.getElementById("maps-div").innerHTML = mapDivHTML;
 }
+
+//trying to have the correct user room on confirmation page (page 5)
+function getRoomNumber(room){
+  document.location.href="RF4thPage.html";
+  document.getElementById("RoomID").innerHTML = "Your rooom, " + room + ", has been reserved";
+}
+
+//trying to have correct time and date on confirmation page (page 5)
+function changeToUserInput(){
+  var time = document.getElementById('TimeID');
+  var date = document.getElementById('DateID');
+  var useTime = document.getElementById('time');
+  var useDate = document.getElementsById('date');
+
+  time.innerHTML = "from " + useTime + " to" + useTime;
+  date.innerHTML = "for " + useDate;
+}
+
+//trying to set reservation page to accurately selected things
+function changeReservations(){
+  document.location.href = "index.html";
+  var time = document.getElementById('TimeID');
+  var date = document.getElementById('DateID');
+  var room = document.getElementById('RoomID');
+  var rev = document.getElementById('theRev');
+
+  rev.innerHTML = "Your room, " + room + ", is reserved from " + time + " to " + time + " on " + date;
+}
+
+//trying to get the org name to pop up if clicked yes
+$("#OrgMaybe").change(function(){
+  if ($(this).val() == "yes"){
+    $('#StuOrgDiv').show();
+    $('#StuOrg').attr('required','');
+  }
+  else{
+    $('#StuOrgDiv').hide();
+    $('#StuOrg').removeAttr('required','');
+  }
+});
+$("#OrgMaybe").trigger("change");
