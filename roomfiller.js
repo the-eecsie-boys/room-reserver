a82f18c623e3f73c81a8bcfb2c4a9a208e4fb6ee
George M <geogeo357@gmail.com>
2019-05-27 16:26:10 -0500

    worked on JS code for adding rooms so that filtering can be done

--- roomfiller.js
+++ roomfiller.js
@@ -1,37 +1,76 @@
 var rooms = [
   {
-    capacity: "30",
-    availability: "1:00 1:00"
+    name: "A110",
+    capacity: "25",
+    availability: "1:00pm-1:50pm"
   },
   {
-    capacity: "30",
-    availability: "1:00 1:00"
+    name: "A119",
+    capacity: "25",
+    availability: "1:00pm-1:50pm"
+  },
+  {
+    name: "A116",
+    capacity: "35",
+    availability: "1:00pm-2:50pm"
+  },
+  {
+    name: "A128",
+    capacity: "45",
+    availability: "1:00pm-1:50pm"
+  },
+  {
+    name: "A120",
+    capacity: "15",
+    availability: "1:00pm-3:50pm"
+  },
+  {
+    name: "A134",
+    capacity: "40",
+    availability: "1:00pm-1:50pm"
+  },
+  {
+    name: "A139",
+    capacity: "45",
+    availability: "1:00pm-1:50pm"
+  },
+  {
+    name: "A136",
+    capacity: "10",
+    availability: "1:00pm-1:50pm"
+  },
+  {
+    name: "A138",
+    capacity: "50",
+    availability: "1:00pm-1:50pm"
+  },
+  {
+    name: "A140",
+    capacity: "15",
+    availability: "1:00pm-1:50pm"
   }
 };
 
 ]
 
 function availablerooms(){
-  var allrooms = document.getElementById("available-rooms");
+  var allrooms = document.getElementById("OpenRooms");
+  allRooms.innerHTML = "";
   if (rooms != null) {
     for (var i=0; i<rooms.length; i++) {
-      var cardWrapper = document.createElement('div');
-      cardWrapper.classList.add("card text-white bg-primary room-info-wing-closeup");
-
-      var cardHeader = document.createElement('div');
-      cardHeader.classList.add("card-header room-info-wing-closeup-header");
-
-      var cardBody = document.createElement('div');
-      cardBody.classlist.add("card-body text-center")
-
-      var cardContents = `
-      <h5 class="card-title">Capacity: ${rooms[i].capacity}</h5>
-      <p class="card-text">${rooms[i].availability}</p>
-      `;
-
-      cardBody.innerHTML = cardContents;
-      allrooms.appendChild(cardWrapper);
+      var cardWrapperHolder = document.createElement('div');
+      cardWrapper.innerHTML = `div class="card text-white bg-primary room-info-wing-closeup">
+      <div class="card-header room-info-wing-closeup-header" onclick="getRoomNumber(this)">${rooms[i].name}</div>
+      <div class="card-body">
+        <h5 class="card-title text-center">Capacity: ${rooms[i].capacity}</h5>
+        <p class="card-text text-center">${rooms[i].availability}</p>
+      </div>`;
+      allrooms.appendChild(cardWrapperHolder);
     }
   }
+  var buttonHTML = `<div id="backBtn">
+        <button type="button" class="btn btn-primary btn-block" onclick="window.location.href='RF2ndPage.html'">Back</button>
+    </div>`;
+  allRooms.innerHTML = allrooms.innerHTML + buttonHTML;
 
 }
